f2bef373e3b20fdb94f8a9a56df2598b45f27a5e
aljaz <aljaz@DESKTOP-JTOE139>
2016-12-18 17:01:11 +0300

    Add project files.

---
+++ MyVariab/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVariab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myValue = 14;
+            Console.WriteLine(myValue);
+            Console.ReadKey();
+
+
+        
+
+        }
+    }
+}
